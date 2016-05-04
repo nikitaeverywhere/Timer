dae303c266352a0eff19cc931ad34174efbaf3f9
ZitRos <zitros.lab@gmail.com>
2016-05-05 00:43:40 +0300

    Release changes, API enhance, readme update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Timer
+# Timer <sup>v1.0.0</sup>
 
 The vanilla ES6 JavaScript simple timer module for any needs. Takes an
 existing HTML element and creates a timer from it.
@@ -31,10 +31,14 @@ Lets say we have this HTML:
 To initialize timer, and, for example, set the count up timer (default one), write the next JavaScript:
 
 ```js
-import Timer from "./Timer"; // directory where timer is stored
+import Timer from `./Timer`; // directory where timer is stored
 
-new Timer(document.querySelector("#timer"), {
-    mask: "" // optional, may not be specified
+let timerElement = document.querySelector(`#timer`),
+    timer = new Timer(timerElement);
+
+// For example, this makes timer start from beginning each click on it 
+timerElement.addEventListener(`click`, () => {
+    timer.reset({ autoStart: false }).start(); // chain sample!
 });
 ```
 
@@ -42,6 +46,10 @@ API
 ---
 
 + [Timer(element, [options])](#timerelement-options)
+    + [reset()](#reset-returns-timer)
+    + [start()](#start-returns-timer)
+    + [stop()](#stop-returns-timer)
+    + [update()](#update-returns-timer)
 
 ##### Timer(element, [options])
 
@@ -53,3 +61,22 @@ Constructor. Initializes the timer over the element.
     + `updateInterval` `{number}` = `1000` - Timer update interval in milliseconds.
     + `type` `{string}` = `"countup"` - Timer type.
     + `initialTime` `{number}` = `0` - Initial time in milliseconds.
+    + `autoStart` `{boolean}` = `true` - Start the timer when constructor instantiated.
+    
+##### reset([options]) `@returns {Timer}`
+
+Resets the timer to it's initial state. Optionally takes `options` which
+are the same as in constructor.
+    
+##### start() `@returns {Timer}`
+
+Start the timer. 
+
+##### stop() `@returns {Timer}`
+
+Stops the timer.
+
+##### update() `@returns {Timer}`
+
+Refreshes timer UI. Not for potential use, as timer calls this function
+each time it updates.
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * The vanilla ES6 JavaScript timer module for any needs.
  * @author Nikita Savchenko
  * @url https://github.com/ZitRos/Timer
+ * @version 1.0.0
  * @param {HTMLElement} element - Element to attach timer to. Inner text of the element will be
  * controlled by Timer module.
  * @param {Object} [options]
@@ -9,25 +10,23 @@
  * @param {number} [options.updateInterval=1000] - Timer update interval in milliseconds.
  * @param {string} [options.type=countup] - Timer type.
  * @param {number} [options.initialTime=0] - Initial time in milliseconds.
+ * @param {boolean} [options.autoStart=true] - Start the timer after constructor is called.
  */
-export default function Timer (element, options = {}) {
+export default function Timer (element, options) {
 
     if (!(element instanceof HTMLElement))
         throw new Error("element must be an HTMLElement:", element);
 
     this.element = element;
-    this.mask = options.mask || "hh:mm:ss";
-    this.updateInterval = options.updateInterval || 1000;
-    this.TYPE = options.type || "countup";
-    this.initialTime = new Date().getTime() + (options.initialTime || 0);
 
     // prevent previously set timers from conflicting
     if (element._ZITROTIMER && element._ZITROTIMER instanceof Timer)
         element._ZITROTIMER.stop();
-    element._ZITROTIMER = this;
+    this.element._ZITROTIMER = this;
 
-    this.interval = setInterval(this.update.bind(this), this.updateInterval);
-    this.update();
+    this.options = options;
+
+    this.reset(options);
 
 }
 
@@ -35,14 +34,73 @@ function pad (val, n = 2) {
     return `000${ val }`.slice(-n);
 }
 
+/**
+ * Stop the timer updates.
+ * @returns {Timer}
+ */
 Timer.prototype.stop = function () {
 
+    if (!this.interval)
+        return this;
+
     clearInterval(this.interval);
+    this.interval = 0;
+    
+    return this;
 
 };
 
 /**
- * @private
+ * Start the timer updates.
+ * @returns {Timer}
+ */
+Timer.prototype.start = function () {
+
+    if (this.interval)
+        this.stop();
+
+    this.interval = setInterval(this.update.bind(this), this.updateInterval);
+    this.update();
+    
+    return this;
+
+};
+
+/**
+ * Start the timer from initial point. Optionally takes options needed to be reassigned.
+ * @param {Object} [options]
+ * @param {string} [options.mask=hh:mm:ss] - Output timer mask.
+ * @param {number} [options.updateInterval=1000] - Timer update interval in milliseconds.
+ * @param {string} [options.type=countup] - Timer type.
+ * @param {number} [options.initialTime=0] - Initial time in milliseconds.
+ * @param {boolean} [options.autoStart=true] - Start the timer after constructor is called.
+ * @returns {Timer}
+ */
+Timer.prototype.reset = function (options = {}) {
+
+    if (options)
+        this.options = Object.assign(this.options, options);
+
+    this.mask = this.options.mask || "hh:mm:ss";
+    this.updateInterval = this.options.updateInterval || 1000;
+    this.TYPE = this.options.type || "countup";
+    this.initialTime = new Date().getTime() + (this.options.initialTime || 0);
+    this.options.autoStart =
+        typeof this.options.autoStart === `undefined` ? true : this.options.autoStart;
+
+    this.stop();
+    this.update();
+
+    if (this.options.autoStart)
+        this.start();
+    
+    return this;
+
+};
+
+/**
+ * Manually updates the timer.
+ * @returns {Timer}
  */
 Timer.prototype.update = function () {
     
@@ -64,5 +122,7 @@ Timer.prototype.update = function () {
         .replace(/h{1,2}/g, p => pad(hours, p.length))
         .replace(/m{1,2}/g, p => pad(minutes, p.length))
         .replace(/s{1,2}/g, p => pad(seconds, p.length));
+    
+    return this;
 
 };
