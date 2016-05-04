e9e9569e86a07139b034bbf0109046efb0749a4c
ZitRos <zitros.lab@gmail.com>
2016-05-04 21:07:17 +0300

    Module upload

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # Timer
-The vanilla ES6 JavaScript timer module for any needs.
+
+The vanilla ES6 JavaScript simple timer module for any needs. Takes an
+existing HTML element and creates a timer from it.
+
+Installation
+------------
+
+You can just take ready-to-use `index.js` file from this repository.
+But the better way would be to clone this repository as a git submodule into your project:
+
+```bash
+cd your/project/directory
+git submodule add https://github.com/ZitRos/Timer.git source/client/js/Timer
+```
+
+**NOTE:** `your/project/directory` is the directory **of your project**, `source/js/Toast`
+is the sub-directory **in your project** where this repository will be cloned.
+
+Usage
+-----
+
+Lets say we have this HTML:
+ 
+```html
+<div>
+    Time since page load: <span id="timer"></span>
+</div>
+```
+
+To initialize timer, and, for example, set the count up timer (default one), write the next JavaScript:
+
+```js
+import Timer from "./Timer"; // directory where timer is stored
+
+new Timer(document.querySelector("#timer"), {
+    updateInterval: 25 // optional, may not be specified
+});
+```
+
+API
+---
+
++ [Timer(element, [options])](#timerelement-options)
+
+##### Timer(element, [options])
+
+Constructor. Initializes the timer over the element.
+
++ `element` - HTML element.
++ `options` - Optional object with additional configuration.
+    + `mask` `{string}` = `"HH:MM:ss"` - Output format.
+    + `updateInterval` `{number}` = `50` - Timer update interval in milliseconds.
+    + `type` `{string}` = `"countup"` - Timer type.
+    + `initialTime` `{number}` = `0` - Initial time in milliseconds.
---
+++ index.js
@@ -0,0 +1,58 @@
+/**
+ * The vanilla ES6 JavaScript timer module for any needs.
+ * @author Nikita Savchenko
+ * @url https://github.com/ZitRos/Timer
+ * @param {HTMLElement} element - Element to attach timer to. Inner text of the element will be
+ * controlled by Timer module.
+ * @param {Object} [options]
+ * @param {string} [options.mask=HH:MM:ss] - Output timer mask.
+ * @param {number} [options.updateInterval=50] - Timer update interval in milliseconds.
+ * @param {string} [options.type=countup] - Timer type.
+ * @param {number} [options.initialTime=0] - Initial time in milliseconds.
+ */
+export default function Timer (element, options = {}) {
+
+    if (!(element instanceof HTMLElement))
+        throw new Error("element must be an HTMLElement:", element);
+
+    this.element = element;
+    this.mask = options.mask || "hh:mm:ss";
+    this.updateInterval = options.updateInterval || 50;
+    this.TYPE = options.type || "countup";
+    this.initialDate = new Date() + (options.initialTime || 0);
+
+    this.interval = setInterval(this.update.bind(this), this.updateInterval);
+
+    this.update();
+
+}
+
+function pad (val, n = 2) {
+    return `000${ val }`.slice(-n);
+}
+
+/**
+ * @private
+ */
+Timer.prototype.update = function () {
+    
+    let time;
+    
+    if (this.TYPE === "countup")
+        time = new Date() - this.initialDate;
+    else {
+        time = Math.max(0, this.initialDate - new Date());
+        if (time === 0)
+            clearInterval(this.interval);
+    }
+    
+    let hours = Math.floor(time / 1000 / 60 / 60),
+        minutes = Math.floor(time / 1000 / 60 - hours),
+        seconds = Math.floor(time / 1000 - hours - minutes);
+
+    this.element.textContent = this.mask
+        .replace(/h{1,2}/g, p => pad(hours, p.length))
+        .replace(/m{1,2}/g, p => pad(minutes, p.length))
+        .replace(/s{1,2}/g, p => pad(seconds, p.length));
+
+};
